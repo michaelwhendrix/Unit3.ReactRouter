2f91bc44157c0a43f1d6da2619b420c45d15e0b4
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-21 11:02:22 -0600

    added react-router and put in the router and route compminents with h1 displays

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div id="container">
-      <h1>Hello React Router!</h1>
       <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <div id="main-section">
+        <Routes>
+          <Route path='/red' element={<h1>Red</h1>}/>
+          <Route path='/blue' element={<h1>Blue</h1>}/>
+        </Routes>
+      </div>
     </div>
     </>
   )
